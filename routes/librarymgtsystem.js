ffd5101767098a95d5cb34e025c72818a021a627
abronoma <trudykingsberry@gmail.com>
2025-03-11 11:06:45 +0000

    made changes to the routes

--- routes/librarymgtsystem.js
+++ routes/librarymgtsystem.js
@@ -6,8 +6,6 @@ lmsRouter.post("/book", addBook);
 
 lmsRouter.get("/books", getBooks);
 
-lmsRouter.get("/books/count", countBook);
-
 lmsRouter.patch("/book/:id", updateBook);
 
 lmsRouter.delete("/book/:id", deleteBook);

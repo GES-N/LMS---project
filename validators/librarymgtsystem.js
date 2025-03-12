cee9ff1ed6d3a6d6431f88ba60f8f4af34cc940e
abronoma <trudykingsberry@gmail.com>
2025-03-12 16:28:30 +0000

    image upload added

--- validators/librarymgtsystem.js
+++ validators/librarymgtsystem.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-export const addBookSchema = Joi.object({
+
 export const addBookSchema = Joi.object({
   title: Joi.string().required(),
   author: Joi.string().required(),
